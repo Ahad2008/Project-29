3f24242d760203cf5408194b581751b18a8eb1a7
Ahad2008 <72267510+Ahad2008@users.noreply.github.com>
2020-12-13 11:01:31 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,9 +1,8 @@
 class Block{
     constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            'restitution':0.4,
+            'friction':0.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
@@ -12,13 +11,13 @@ class Block{
       }
       display(){
         var pos =this.body.position;
-        //var angle = this.body.angle;
+        var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
-       // rotate(angle);
+        rotate(angle);
         rectMode(CENTER);
-        fill("yellow");
-        rect(pos.x, pos.y, this.width, this.height);
+        fill("red");
+        rect(0, 0, this.width, this.height);
         pop();
       }
 }
--- sketch.js
+++ sketch.js
@@ -5,8 +5,13 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var ground1, ground2;
-var block1, block2, block3, block4, block5, block6, block7, block8, block9, block10, block11, block12, block13, block14, block15, block16;
-//var slingshot; 
+var block1, block2, block3, block4, block5, block6, block7, block8, block9, block10, block11, block12, block13, block14, block15, block16, block17, block18;
+var polygon, polygonImg;
+var slingshot; 
+
+function preload() {
+    polygonImg = loadImage("polygon.png");
+}
 
 function setup(){
     createCanvas(1200,800);
@@ -18,28 +23,33 @@ function setup(){
     ground2 = new Ground(500, 500, 300, 20);
     ground3 = new Ground(1000, 250, 300, 20);
 
-    block1 = new Block(700,450,70,70);
-    block2 = new Block(920,450,70,70);
-    block3 = new Block(700,450,70,70);
-    block4 = new Block(920,450,70,70);
-    block5 = new Block(810,450,70,70);
-    block6 = new Block(700,450,70,70);
-    block7 = new Block(920,450,70,70);
-    block8 = new Block(700,450,70,70);
-    block9 = new Block(920,450,70,70);
-    block10 = new Block(810,450,70,70);
-    block11 = new Block(700,450,70,70);
-    block12 = new Block(920,450,70,70);
-    block13 = new Block(700,450,70,70);
-    block14 = new Block(920,450,70,70);
-    block15 = new Block(810,450,70,70);
-    block16 = new Block(810,450,70,70);
+    block1 = new Block(430,450,30,40);
+    block2 = new Block(460,450,30,40);
+    block3 = new Block(490,450,30,40);
+    block4 = new Block(520,450,30,40);
+    block5 = new Block(550,450,30,40);
+    block6 = new Block(460,410,30,40);
+    block7 = new Block(490,410,30,40);
+    block8 = new Block(520,410,30,40);
+    block9 = new Block(490,380,30,40);
+    block10 = new Block(930,200,30,40);
+    block11 = new Block(960,200,30,40);
+    block12 = new Block(990,200,30,40);
+    block13 = new Block(1020,200,30,40);
+    block14 = new Block(1050,200,30,40);
+    block15 = new Block(960,160,30,40);
+    block16 = new Block(990,160,30,40);
+    block17 = new Block(1020,160,30,40);
+    block18 = new Block(990,120,30,40);
 
-    //slingshot = new Slingshot(bird.body,{x:200, y:50});
+    polygon = Bodies.circle(50, 200, 20);
+    World.add(world, polygon);
+
+    slingshot = new Slingshot(this.polygon,{x:200, y:200});
 }
 
 function draw(){
-    background(0);
+    background("green");
     Engine.update(engine);
 
     ground1.display();
@@ -62,14 +72,19 @@ function draw(){
     block14.display();
     block15.display();
     block16.display();
+    block17.display();
+    block18.display();
 
-    //slingshot.display();    
+    imageMode(CENTER);
+    image(polygonImg, polygon.position.x, polygon.position.y, 40, 40);
+
+    slingshot.display();    
 }
 
-//function mouseDragged(){
-//    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-//}
+function mouseDragged(){
+    Matter.Body.setPosition(this.polygon, {x: mouseX , y: mouseY});
+}
 
-//function mouseReleased(){
-//    slingshot.fly();
-//}
+function mouseReleased(){
+    slingshot.fly();
+}
